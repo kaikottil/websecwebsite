b6204e993c4b2db3c8920daac74c05f3ff0406e1
kaikottil <akshaykaikottil@gmail.com>
2022-12-02 15:33:37 -0500

    added hardcoded login creds. Need to update to db in ver 2.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,19 @@ def login():  # put application's code here
     resp.headers['X-XSS-Protection'] = '0'
     print("login page loaded")
     return resp
+
+@app.route('/loginCheck', methods=["GET", "POST"])
+def loginCheck():  # put application's code here
+    UserName = request.args.get('email')
+    pswd = request.args.get('pwd')
+    print(UserName)
+    print(pswd)
+    if (UserName=="user@bot.com" and pswd=="temp1234"):
+        resp = make_response(render_template('success.html'))
+        return resp
+    else:
+        resp = make_response(render_template('error.html'))
+        return resp
 
 if __name__ == '__main__':
     app.run()
